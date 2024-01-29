808989161415367d5d62ba44dc3d52601e7ce3f2
tossudev <tossudev@gmail.com>
2024-01-29 16:03:09 +0200

    Tidied up background.gd
    Mainly added comments for clarity

--- scripts/background.gd
+++ scripts/background.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-const BG_TEXTURE_WIDTH: int = 288 # Used for repeating textures infinitely
+# Texture sizes, used to repeat textures infinitely.
+const BG_TEXTURE_WIDTH: int = 288
+
+# Speed values for different layers
 const CLOUDS_SPEED: float = -10.0
 const CLOUDS_FG_SPEED: float = -5.0
 const WATER_SPEED: float = -15.0
 const WATER_FG_SPEED: float = -20.0
 
-
+# Node references
 var clouds: Object
 var clouds_fg: Object
 var water: Object
@@ -16,11 +19,21 @@ var moving_objects: Array = []
 
 
 func _ready():
+	_init_nodes()
+
+
+func _process(delta):
+	_update_background(delta)
+
+
+func _init_nodes() -> void:
+	# Set all node references
 	clouds = $Clouds
 	clouds_fg = $CloudsFG
 	water = $Water
 	water_fg = $WaterFG
 	
+	# Add moving objects to a list
 	moving_objects = [
 		clouds,
 		clouds_fg,
@@ -29,17 +42,15 @@ func _ready():
 	]
 
 
-func _process(delta):
-	_update_background(delta)
-
-
 func _update_background(delta: float) -> void:
+	# Move all layers at their own speeds.
 	clouds.position.x += CLOUDS_SPEED * delta
 	clouds_fg.position.x += CLOUDS_FG_SPEED * delta
 	
 	water.position.x += WATER_SPEED * delta
 	water_fg.position.x += WATER_FG_SPEED * delta
 	
+	# When layer x-position exceeds its own width, set layer back to 0 on the x-axis.
 	for bg_node in moving_objects:
 		if bg_node.position.x <= -BG_TEXTURE_WIDTH:
 			bg_node.position.x = 0
